35ac4c7d4ada6d622096d33fb1b133042b57411a
Lara <laratozkoparan5@gmail.com>
2024-10-05 16:19:25 +0300

    Add files via upload
    first commit (v1)

---
+++ myFirstCarGameProject.py
@@ -0,0 +1,131 @@
+
+# import pygame
+# import sys
+
+# # Pygame'i başlat
+# pygame.init()
+
+# # Renkler
+# BEYAZ = (255, 255, 255)
+
+# # Pencere boyutu
+# GENISLIK, YUKSEKLIK = 600, 400
+# ekran = pygame.display.set_mode((GENISLIK, YUKSEKLIK))
+# pygame.display.set_caption("Araba Oyunu")
+
+# # Saat ayarı (FPS için)
+# saat = pygame.time.Clock()
+
+# # Araba görselini yükle
+# araba_resmi = pygame.image.load('car.png')
+# araba_resmi = pygame.transform.scale(araba_resmi, (100, 150))  # Boyutu ayarla
+
+# arka_plan = pygame.image.load('_car.png')
+# arka_plan = pygame.transform.scale(arka_plan, (GENISLIK, YUKSEKLIK))  # Boyutu ayarla
+
+# # Araba başlangıç pozisyonu
+# araba_x = GENISLIK//2 - 25
+# araba_y = YUKSEKLIK - 110
+# araba_hizi = 5
+
+# # Oyun döngüsü
+# calisiyor = True
+# while calisiyor:
+#     # Olayları kontrol et
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+
+#     # Tuşlarla hareket
+#     tuslar = pygame.key.get_pressed()
+#     if tuslar[pygame.K_LEFT] and araba_x > 0:
+#         araba_x -= araba_hizi
+#     if tuslar[pygame.K_RIGHT] and araba_x < GENISLIK - 50:
+#         araba_x += araba_hizi
+#     if tuslar[pygame.K_UP] and araba_y > 0:
+#         araba_y -= araba_hizi
+#     if tuslar[pygame.K_DOWN] and araba_y < YUKSEKLIK - 100:
+#         araba_y += araba_hizi
+
+#     # Ekranı beyaz renkle temizle
+#     # ekran.fill(BEYAZ)
+#     # ekran.fill(arka_plan)
+
+#     # Arabayı ekranda çiz
+#     ekran.blit(araba_resmi, (araba_x, araba_y))
+#     ekran.blit(arka_plan, (0,0))
+
+#     # Ekranı güncelle
+#     pygame.display.flip()
+
+#     # FPS ayarı
+#     saat.tick(60)
+    
+
+import pygame
+import sys
+
+# Pygame'i başlat
+pygame.init()
+
+# Renkler
+BEYAZ = (255, 255, 255)
+
+# Pencere boyutu
+GENISLIK, YUKSEKLIK = 600, 400
+ekran = pygame.display.set_mode((GENISLIK, YUKSEKLIK))
+pygame.display.set_caption("Araba Oyunu")
+
+# Saat ayarı (FPS için)
+saat = pygame.time.Clock()
+
+# Araba görselini yükle
+araba_resmi = pygame.image.load('car.png')
+araba_resmi = pygame.transform.scale(araba_resmi, (50, 100))  # Boyutu ayarla
+
+arka_plan = pygame.image.load("carpark.png")
+arka_plan = pygame.transform.scale(arka_plan, (GENISLIK, YUKSEKLIK))
+
+
+# Araba başlangıç pozisyonu
+araba_x = GENISLIK//2 - 25
+araba_y = YUKSEKLIK - 110
+araba_hizi = 5
+
+# Oyun döngüsü
+calisiyor = True
+while calisiyor:
+    # Olayları kontrol et
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Tuşlarla hareket
+    tuslar = pygame.key.get_pressed()
+    if tuslar[pygame.K_LEFT] and araba_x > 0:
+        araba_x -= araba_hizi
+    if tuslar[pygame.K_RIGHT] and araba_x < GENISLIK - 50:
+        araba_x += araba_hizi
+    if tuslar[pygame.K_UP] and araba_y > 0:
+        araba_y -= araba_hizi
+    if tuslar[pygame.K_DOWN] and araba_y < YUKSEKLIK - 100:
+        araba_y += araba_hizi
+
+    # Ekranı beyaz renkle temizle
+    ekran.fill(BEYAZ)
+
+    # Arabayı ekranda çiz
+    ekran.blit(arka_plan, (0,0))
+    ekran.blit(araba_resmi, (araba_x, araba_y))
+
+    # Ekranı güncelle
+    pygame.display.flip()
+
+    # FPS ayarı
+    saat.tick(60)
+    
+
+
+
